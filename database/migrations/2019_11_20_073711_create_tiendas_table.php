fa60072682b3b722188eb0f1e060de7f9c2066de
samuelroam <samuelroam@gmail.com>
2019-11-20 09:00:50 +0100

    añadidas migraciones, pre-composer update

---
+++ database/migrations/2019_11_20_073711_create_tiendas_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTiendasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tiendas', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name',20);
+            $table->longtext('description');
+            $table->string('password',255);
+            $table->string('email',50);
+            $table->integer('likes',11);
+            $table->timestamp('created_at');
+            $table->timestamp('updated_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tiendas');
+    }
+}
---
+++ database/migrations/2019_11_20_073730_create_productos_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name',30);
+            $table->longtext('description');
+            $table->integer('stock');
+            $table->double('price', 5,2);
+            $table->timestamp('created_at');
+            $table->timestamp('updated_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('productos');
+    }
+}
